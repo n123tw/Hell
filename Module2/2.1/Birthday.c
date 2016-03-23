31d9cda741da76f0047590f50d5ee836500712d0
Daniil Lazarev <okroshla97@bk.ru>
2016-03-23 23:55:25 +0300

    Hello

--- Module2/2.1/Birthday.c
+++ Module2/2.1/Birthday.c
@@ -5,6 +5,6 @@ int main(void)
 	int day, month;
 	day = 21;
 	month = 11;
-	printf("%d%d/%d%d\n", day/10, day%10, month/10, month%10); /* day/10 - деление переменной day на 10, day%10 - остаток от деления переменной day на 10 */
+	printf("%d%d/%d%d\n", day/10, day%10, month/10, month%10); 
 	return 0;
 }
