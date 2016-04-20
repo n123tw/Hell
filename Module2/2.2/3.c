a9aedaf44bb78a50341239c8eee0a1e585a16f2c
Daniil Lazarev <okroshla97@bk.ru>
2016-04-20 23:33:47 +0300

    asd

---
+++ Module2/2.2/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+char string[100000];
+int size;
+printf("Введите строку: \n");
+scanf("%s", string);
+size=strlen(string);
+printf("Резмер введённой строки %d байт\n", size);
+  return 0;
+}
