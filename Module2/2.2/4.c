90a299f45606768c3acd6e282a09f8b997fa3d54
n123tw <okroshka97@bk.ru>
2016-04-26 21:02:46 +0300

    Update 4.c

--- Module2/2.2/4.c
+++ Module2/2.2/4.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
-#include <string.h>
 
+#include <stdio.h>
+#define SPACE ' ' 
 int main (void)
- { int cache,i;
-   char string[5000];
-   printf("Введите буквы,увеличить регистр которых вы хотите  \n");
-   scanf("%s",string);
-   cache=strlen(string);
-   i=0;
-   while (i<cache)
-    { string[i]=string[i]-32;
-      i=i+1; }
-     printf("%s \n",string);
-   return 0;
-  }
+{
+char ch;
+printf("Введите буквы,увеличить регистр которых вы хотите  \n");
+ch = getchar ();
+while (ch != '\n')
+{
+if (ch == SPACE)
+putchar (ch);
+else
+putchar (ch - 32);
+ch = getchar ( ) ;
+}
+putchar (ch);
+return 0;
+}
