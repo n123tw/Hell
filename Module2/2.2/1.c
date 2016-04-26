a872d22c36f197125510f588505089cab7d610d7
n123tw <okroshka97@bk.ru>
2016-04-26 21:17:51 +0300

    Update 1.c

--- Module2/2.2/1.c
+++ Module2/2.2/1.c
@@ -5,8 +5,8 @@ int year, month, day;
 printf("Введите число дней\n");
 scanf("%d", &day);
 year=day/365;
-month=(day-year)/30;
-day=day-30*month;
+month=(day-year*365)/30;
+day=day-year*365-month*30;
 printf("Это %d лет, %d месяцев, %d дней", year, month, day);
 return 0;
 }
