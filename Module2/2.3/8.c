7842e0f4d5170566ccef89dcc71446bd340172d5
n123tw <okroshka97@bk.ru>
2016-04-26 21:31:53 +0300

    Create 8.c

---
+++ Module2/2.3/8.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+float a,b, result;
+char command;
+printf("Введите команду (например, '7+3'):\n");
+scanf("%f%c%f", &a,&command,&b);
+switch(command)
+{
+case '+': result=a+b;
+          break;
+case '-': result=a-b;
+          break;
+case '*': result=a*b;
+          break;
+case '/': result=a/b;
+          break;
+case '^': result=pow(a,b);
+          break;
+case '%': printf("Введите число, процент от которого вы хотите узнать:\n"); //как поменять порядок?
+
+result=a/b*100;
+
+          break;
+}
+printf("=%.9f\n", result);
+return 0;
+}
