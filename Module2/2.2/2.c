f01d057b01ace310cf557ad1e02040a78b9a2a38
n123tw <okroshka97@bk.ru>
2016-06-05 01:15:38 +0300

    Update 2.c

--- Module2/2.2/2.c
+++ Module2/2.2/2.c
@@ -5,10 +5,10 @@ int main()
 printf("    | .0 | .1 | .2 | .3 | .4 | .5 | .6 | .7 | .8 | .9 | .A | .B | .C | .D | .E | .F |\n"); 
 printf(" 0. | NUL| SOH| STX| ETX| EOT| ENQ| ACK| BEL| BS | HT | LF | VT | FF | CR | SO | SI |\n");
 printf(" 1. | DLE| DC1| DC2| DC3| DC4| NAK| SYN| ETB| CAN| EM | SUB| ESC| FS | GS | RS | US |\n");
-printf(" 2. |    |  ! | \" |  # |  $ | %% |  & |  ' |  ( |  ) |  * |  + |  , |  - |  . |  / |\n"); 
+printf(" 2. |    |  ! |  \" |  # |  $ |  %% |  & |  ' |  ( |  ) |  * |  + |  , |  - |  . |  / |\n"); 
 printf(" 3. |  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  : |  ; |  < |  = |  > |  ? |\n"); 
 printf(" 4. |  @ |  A |  B |  C |  D |  E |  F |  G |  H |  I |  J |  K |  L |  M |  N |  O |\n");
-printf(" 5. |  P |  Q |  R |  S |  T |  U |  V |  W |  X |  Y |  Z |  [ | \\ |  ] |  ^ |  _ |\n");
+printf(" 5. |  P |  Q |  R |  S |  T |  U |  V |  W |  X |  Y |  Z |  [ |  \\ |  ] |  ^ |  _ |\n");
 printf(" 6. |  ` |  a |  b |  c |  d |  e |  f |  g |  h |  i |  j |  k |  l |  m |  n |  o |\n");
 printf(" 7. |  p |  q |  r |  s |  t |  u |  v |  w |  x |  y |  z |  { |  | |  } |  ~ | DEL|\n");
 return 0;
