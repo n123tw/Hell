39cfb89ecf5244f9b2bc6d09850f940d2d9a7874
n123tw <okroshka97@bk.ru>
2016-06-07 01:38:02 +0300

    Update Ten.c

--- Module2/2.2/Ten.c
+++ Module2/2.2/Ten.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 int main()
 {
-char s1;
-int s2;
-float s3;
-double s4;
-printf("Тип char имеет размер %d байт(а, ов).\n", sizeof(s1));
-printf("Тип int имеет размер %d байт(а, ов).\n", sizeof(s2));
-printf("Тип float имеет размер %d байт(а, ов).\n", sizeof(s3));
-printf("Тип double имеет размер %d байт(а, ов).\n", sizeof(s4));
+printf("Тип char имеет размер %d байт(а, ов).\n", sizeof(char));
+printf("Тип int имеет размер %d байт(а, ов).\n", sizeof(int));
+printf("Тип float имеет размер %d байт(а, ов).\n", sizeof(float));
+printf("Тип double имеет размер %d байт(а, ов).\n", sizeof(double));
 return 0;
 }
