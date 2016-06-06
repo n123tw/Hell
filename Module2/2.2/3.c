159a78b9b70e0baf12d7d3b3e5abd640e0f84023
n123tw <okroshka97@bk.ru>
2016-06-07 01:34:20 +0300

    Update 3.c

--- Module2/2.2/3.c
+++ Module2/2.2/3.c
@@ -7,6 +7,6 @@ int size;
 printf("Введите строку: \n");
 scanf("%s", string);
 size=strlen(string);
-printf("Резмер введённой строки %d байт\n", size);
+printf("Резмер введённой строки %d байт\n", size+1);
   return 0;
 }
